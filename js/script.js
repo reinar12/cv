183be8e8187746b51aef6b5b68c6cd09e714d4c6
reinar12 <84057487+reinar12@users.noreply.github.com>
2022-03-06 01:47:57 -0300

    fix AM clock bug

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function realtimeClock() {
 
     var amPm = (hours < 12 ) ? "AM" : "PM";
     
-    hours = (hours < 12) ? hours - 12 : hours;
+    hours = (hours > 12) ? hours - 12 : hours;
 
     hours = ("0" + hours).slice(-2);
     minutes = ("0" + minutes).slice(-2);
